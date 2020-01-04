24ed7cfa065cbd729ff8b874c9cafe4a543e1253
skhan59bbk <skhan59@dcs.bbk.ac.uk>
2020-01-04 19:12:42 +0000

    Final commit

--- test_cities.py
+++ test_cities.py
@@ -13,8 +13,6 @@ def test_compute_total_distance():
                  ('Georgia', 'Atlanta', 33.76, -84.39),\
                  ('Florida', 'Tallahassee', 30.45, -84.27)]
     
-    full_map = read_cities('C:\\Users\\samee\\Documents\\POP1\\pop-one-project-skhan59\\city-data.txt')
-    
     try:
         assert compute_total_distance(road_map1) == pytest.approx(9.386+18.496+10.646, 0.01)
     except Exception as e:
@@ -23,7 +21,7 @@ def test_compute_total_distance():
     assert type(road_map1) is list
     assert compute_total_distance(road_map1) > 0.0
     assert compute_total_distance(road_map1) < compute_total_distance(road_map2)
-    assert compute_total_distance(full_map) == pytest.approx(1060.00, 2)
+    assert compute_total_distance(road_map2) > compute_total_distance(road_map1)
     
 
 def test_swap_cities():
@@ -31,16 +29,20 @@ def test_swap_cities():
                 ("Delaware", "Dover", 39.161921, -75.526755),\
                 ("Minnesota", "Saint Paul", 44.95, -93.094)]
     
-    full_map = read_cities('C:\\Users\\samee\\Documents\\POP1\\pop-one-project-skhan59\\city-data.txt')
+    road_map2 = [("Kentucky", "Frankfort", 38.197274, -84.86311),\
+                 ("Delaware", "Dover", 39.161921, -75.526755),\
+                 ("Minnesota", "Saint Paul", 44.95, -93.094),\
+                 ('Georgia', 'Atlanta', 33.76, -84.39),\
+                 ('Florida', 'Tallahassee', 30.45, -84.27)]
     
     try:
-        assert type(swap_cities(full_map,1,2)) is tuple
-        assert type(swap_cities(full_map, 1, 2)[1]) is float
-        assert type(swap_cities(full_map, 1, 2)[0]) is list
-        assert swap_cities(full_map, 2, 2)[0] == full_map
-        assert full_map[0] != swap_cities(full_map, 0, 1)[0][0]
-        assert len(full_map) == len(swap_cities(full_map,0,2)[0])
-        assert swap_cities(full_map,0,3)[1] == compute_total_distance(swap_cities(full_map,0,3)[0])
+        assert type(swap_cities(road_map2,1,2)) is tuple
+        assert type(swap_cities(road_map1, 1, 2)[1]) is float
+        assert type(swap_cities(road_map2, 1, 2)[0]) is list
+        assert swap_cities(road_map2, 2, 2)[0] == road_map2
+        assert road_map2[0] != swap_cities(road_map2, 0, 1)[0][0]
+        assert len(road_map2) == len(swap_cities(road_map2,0,2)[0])
+        assert swap_cities(road_map1,0,3)[1] == compute_total_distance(swap_cities(road_map1,0,3)[0])
     except Exception as e:
         print(str(e))
 
@@ -49,11 +51,34 @@ def test_shift_cities():
                 ("Delaware", "Dover", 39.161921, -75.526755),\
                 ("Minnesota", "Saint Paul", 44.95, -93.094)]
     
-    full_map = read_cities('C:\\Users\\samee\\Documents\\POP1\\pop-one-project-skhan59\\city-data.txt')
+    road_map2 = [("Kentucky", "Frankfort", 38.197274, -84.86311),\
+                 ("Delaware", "Dover", 39.161921, -75.526755),\
+                 ("Minnesota", "Saint Paul", 44.95, -93.094),\
+                 ('Georgia', 'Atlanta', 33.76, -84.39),\
+                 ('Florida', 'Tallahassee', 30.45, -84.27)]
+
     
     assert type(shift_cities(road_map1)) is list
     assert compute_total_distance(shift_cities(road_map1)) == compute_total_distance(road_map1)
     assert road_map1[0] != shift_cities(road_map1)[0]
     assert shift_cities(road_map1)[0] == road_map1[len(road_map1)-1]
-    assert shift_cities(full_map)[0] == full_map[len(full_map)-1]
+    assert shift_cities(road_map2)[0] == road_map2[len(road_map2)-1]
+    
+def test_distances_and_limits():
+
+    road_map2 = [("Kentucky", "Frankfort", 38.197274, -84.86311),\
+                 ("Delaware", "Dover", 39.161921, -75.526755),\
+                 ("Minnesota", "Saint Paul", 44.95, -93.094),\
+                 ('Georgia', 'Atlanta', 33.76, -84.39),\
+                 ('Florida', 'Tallahassee', 30.45, -84.27)]
+    
+    distance_list = distances_and_limits(road_map2)[0]
+    
+    assert len(distance_list) == len(road_map2)
+    assert distances_and_limits(road_map2)[1] >= -90
+    assert distances_and_limits(road_map2)[2] <= 90
+    assert distances_and_limits(road_map2)[3] >= -180
+    assert distances_and_limits(road_map2)[4] <= 180
+    
+    
     
